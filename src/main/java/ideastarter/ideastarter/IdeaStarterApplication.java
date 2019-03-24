0e6ac0aef55ded7b7c6f21e840600bfff37dbe16
Aleksandar Rachev <rachev.96@abv.bg>
2019-03-24 20:07:53 +0200

    Project created

---
+++ src/main/java/ideastarter/ideastarter/IdeaStarterApplication.java
@@ -0,0 +1,13 @@
+package ideastarter.ideastarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdeaStarterApplication.class, args);
+    }
+
+}
